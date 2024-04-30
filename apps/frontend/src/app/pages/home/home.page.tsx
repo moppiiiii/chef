df7f5f21e9884d2e5973f7b80fb6fbfb757db409
moppiiiii <tnktmhssrc@gmail.com>
2024-04-30 20:05:33 +0900

    style: header-styling

--- apps/frontend/src/app/pages/home/home.page.tsx
+++ apps/frontend/src/app/pages/home/home.page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { useDocumentTitle } from '../../contexts/document-title.context';
+import { Header } from '../../components/parts/header/header.parts';
 
 export const HomePage: React.FC = () => {
   useDocumentTitle('Home');
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Header />
+      HomePage
+    </div>
+  );
 };
