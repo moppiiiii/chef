6adff618e47fdea091d2f6e81613eb5eaf5f2608
moppiiiii <tnktmhssrc@gmail.com>
2024-05-11 14:01:24 +0900

    feat: add recipe registration page

---
+++ apps/frontend/src/app/pages/recipe-registration/recipe-registration.page.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { RecipeRegistrationTemplate } from '../../components/templates/recipe-registration/recipe-registration.template';
+
+export const RecipeRegistrationPage: React.FC = () => {
+  return <RecipeRegistrationTemplate />;
+};
